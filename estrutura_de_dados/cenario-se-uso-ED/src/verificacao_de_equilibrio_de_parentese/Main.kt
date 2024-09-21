834f6127aef055622e32bd10bf6fffbefaf53ba2
Caio Ferreira <caioferreiraadev@gmail.com>
2024-09-21 01:16:30 -0300

    Delete estrutura_de_dados/cenario-se-uso-ED directory

--- estrutura_de_dados/cenario-se-uso-ED/src/verificacao_de_equilibrio_de_parentese/Main.kt
+++
@@ -1,8 +0,0 @@
-package verificacao_de_equilibrio_de_parentese
-
-fun main() {
-    val expressaoNumerica = "(5*9) + (12-9)*(4+5)"
-    val expressao = Expressao()
-
-    expressao.validaExpressao(expressaoNumerica)
-}
